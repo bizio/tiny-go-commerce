9dc0d3d268e49457a9a28bba23fddd5cf2afaa47
Fabrizio Manunta <bizio@users.noreply.github.com>
2026-02-26 14:25:10 +0100

    Added: customer signup

--- pkg/user/v1/user/v1/user.pb.go
+++ pkg/user/v1/user/v1/user.pb.go
@@ -404,6 +404,120 @@ func (x *CreateUserResponse) GetUser() *User {
 	return nil
 }
 
+// Request for customer signup
+type SignupRequest struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Email         string                 `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
+	Password      string                 `protobuf:"bytes,2,opt,name=password,proto3" json:"password,omitempty"`
+	FirstName     string                 `protobuf:"bytes,3,opt,name=first_name,json=firstName,proto3" json:"first_name,omitempty"`
+	LastName      string                 `protobuf:"bytes,4,opt,name=last_name,json=lastName,proto3" json:"last_name,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *SignupRequest) Reset() {
+	*x = SignupRequest{}
+	mi := &file_user_v1_user_proto_msgTypes[6]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *SignupRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*SignupRequest) ProtoMessage() {}
+
+func (x *SignupRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_user_v1_user_proto_msgTypes[6]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use SignupRequest.ProtoReflect.Descriptor instead.
+func (*SignupRequest) Descriptor() ([]byte, []int) {
+	return file_user_v1_user_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *SignupRequest) GetEmail() string {
+	if x != nil {
+		return x.Email
+	}
+	return ""
+}
+
+func (x *SignupRequest) GetPassword() string {
+	if x != nil {
+		return x.Password
+	}
+	return ""
+}
+
+func (x *SignupRequest) GetFirstName() string {
+	if x != nil {
+		return x.FirstName
+	}
+	return ""
+}
+
+func (x *SignupRequest) GetLastName() string {
+	if x != nil {
+		return x.LastName
+	}
+	return ""
+}
+
+// Response for customer signup
+type SignupResponse struct {
+	state         protoimpl.MessageState `protogen:"open.v1"`
+	Id            int64                  `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
+	unknownFields protoimpl.UnknownFields
+	sizeCache     protoimpl.SizeCache
+}
+
+func (x *SignupResponse) Reset() {
+	*x = SignupResponse{}
+	mi := &file_user_v1_user_proto_msgTypes[7]
+	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+	ms.StoreMessageInfo(mi)
+}
+
+func (x *SignupResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*SignupResponse) ProtoMessage() {}
+
+func (x *SignupResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_user_v1_user_proto_msgTypes[7]
+	if x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use SignupResponse.ProtoReflect.Descriptor instead.
+func (*SignupResponse) Descriptor() ([]byte, []int) {
+	return file_user_v1_user_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *SignupResponse) GetId() int64 {
+	if x != nil {
+		return x.Id
+	}
+	return 0
+}
+
 var File_user_v1_user_proto protoreflect.FileDescriptor
 
 const file_user_v1_user_proto_rawDesc = "" +
@@ -438,11 +552,20 @@ const file_user_v1_user_proto_rawDesc = "" +
 	"\tlast_name\x18\x04 \x01(\tB\a\xfaB\x04r\x02\x10\x01R\blastName\x12*\n" +
 	"\x04role\x18\x03 \x01(\tB\x16\xfaB\x13r\x11R\bCUSTOMERR\x05ADMINR\x04role\"7\n" +
 	"\x12CreateUserResponse\x12!\n" +
-	"\x04user\x18\x01 \x01(\v2\r.user.v1.UserR\x04user2\x92\x01\n" +
+	"\x04user\x18\x01 \x01(\v2\r.user.v1.UserR\x04user\"\xa1\x01\n" +
+	"\rSignupRequest\x12\x1d\n" +
+	"\x05email\x18\x01 \x01(\tB\a\xfaB\x04r\x02`\x01R\x05email\x12#\n" +
+	"\bpassword\x18\x02 \x01(\tB\a\xfaB\x04r\x02\x10\bR\bpassword\x12&\n" +
+	"\n" +
+	"first_name\x18\x03 \x01(\tB\a\xfaB\x04r\x02\x10\x01R\tfirstName\x12$\n" +
+	"\tlast_name\x18\x04 \x01(\tB\a\xfaB\x04r\x02\x10\x01R\blastName\" \n" +
+	"\x0eSignupResponse\x12\x0e\n" +
+	"\x02id\x18\x01 \x01(\x03R\x02id2\xcd\x01\n" +
 	"\vUserService\x12<\n" +
 	"\aGetUser\x12\x17.user.v1.GetUserRequest\x1a\x18.user.v1.GetUserResponse\x12E\n" +
 	"\n" +
-	"CreateUser\x12\x1a.user.v1.CreateUserRequest\x1a\x1b.user.v1.CreateUserResponseB6Z4github.com/optimatelabs/tiny-go-commerce/pkg/user/v1b\x06proto3"
+	"CreateUser\x12\x1a.user.v1.CreateUserRequest\x1a\x1b.user.v1.CreateUserResponse\x129\n" +
+	"\x06Signup\x12\x16.user.v1.SignupRequest\x1a\x17.user.v1.SignupResponseB6Z4github.com/optimatelabs/tiny-go-commerce/pkg/user/v1b\x06proto3"
 
 var (
 	file_user_v1_user_proto_rawDescOnce sync.Once
@@ -456,7 +579,7 @@ func file_user_v1_user_proto_rawDescGZIP() []byte {
 	return file_user_v1_user_proto_rawDescData
 }
 
-var file_user_v1_user_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_user_v1_user_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_user_v1_user_proto_goTypes = []any{
 	(*User)(nil),               // 0: user.v1.User
 	(*Address)(nil),            // 1: user.v1.Address
@@ -464,6 +587,8 @@ var file_user_v1_user_proto_goTypes = []any{
 	(*GetUserResponse)(nil),    // 3: user.v1.GetUserResponse
 	(*CreateUserRequest)(nil),  // 4: user.v1.CreateUserRequest
 	(*CreateUserResponse)(nil), // 5: user.v1.CreateUserResponse
+	(*SignupRequest)(nil),      // 6: user.v1.SignupRequest
+	(*SignupResponse)(nil),     // 7: user.v1.SignupResponse
 }
 var file_user_v1_user_proto_depIdxs = []int32{
 	1, // 0: user.v1.User.address:type_name -> user.v1.Address
@@ -471,10 +596,12 @@ var file_user_v1_user_proto_depIdxs = []int32{
 	0, // 2: user.v1.CreateUserResponse.user:type_name -> user.v1.User
 	2, // 3: user.v1.UserService.GetUser:input_type -> user.v1.GetUserRequest
 	4, // 4: user.v1.UserService.CreateUser:input_type -> user.v1.CreateUserRequest
-	3, // 5: user.v1.UserService.GetUser:output_type -> user.v1.GetUserResponse
-	5, // 6: user.v1.UserService.CreateUser:output_type -> user.v1.CreateUserResponse
-	5, // [5:7] is the sub-list for method output_type
-	3, // [3:5] is the sub-list for method input_type
+	6, // 5: user.v1.UserService.Signup:input_type -> user.v1.SignupRequest
+	3, // 6: user.v1.UserService.GetUser:output_type -> user.v1.GetUserResponse
+	5, // 7: user.v1.UserService.CreateUser:output_type -> user.v1.CreateUserResponse
+	7, // 8: user.v1.UserService.Signup:output_type -> user.v1.SignupResponse
+	6, // [6:9] is the sub-list for method output_type
+	3, // [3:6] is the sub-list for method input_type
 	3, // [3:3] is the sub-list for extension type_name
 	3, // [3:3] is the sub-list for extension extendee
 	0, // [0:3] is the sub-list for field type_name
@@ -491,7 +618,7 @@ func file_user_v1_user_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: unsafe.Slice(unsafe.StringData(file_user_v1_user_proto_rawDesc), len(file_user_v1_user_proto_rawDesc)),
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
--- pkg/user/v1/user/v1/user.pb.validate.go
+++ pkg/user/v1/user/v1/user.pb.validate.go
@@ -989,3 +989,300 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = CreateUserResponseValidationError{}
+
+// Validate checks the field values on SignupRequest with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *SignupRequest) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on SignupRequest with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in SignupRequestMultiError, or
+// nil if none found.
+func (m *SignupRequest) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *SignupRequest) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if err := m._validateEmail(m.GetEmail()); err != nil {
+		err = SignupRequestValidationError{
+			field:  "Email",
+			reason: "value must be a valid email address",
+			cause:  err,
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetPassword()) < 8 {
+		err := SignupRequestValidationError{
+			field:  "Password",
+			reason: "value length must be at least 8 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetFirstName()) < 1 {
+		err := SignupRequestValidationError{
+			field:  "FirstName",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if utf8.RuneCountInString(m.GetLastName()) < 1 {
+		err := SignupRequestValidationError{
+			field:  "LastName",
+			reason: "value length must be at least 1 runes",
+		}
+		if !all {
+			return err
+		}
+		errors = append(errors, err)
+	}
+
+	if len(errors) > 0 {
+		return SignupRequestMultiError(errors)
+	}
+
+	return nil
+}
+
+func (m *SignupRequest) _validateHostname(host string) error {
+	s := strings.ToLower(strings.TrimSuffix(host, "."))
+
+	if len(host) > 253 {
+		return errors.New("hostname cannot exceed 253 characters")
+	}
+
+	for _, part := range strings.Split(s, ".") {
+		if l := len(part); l == 0 || l > 63 {
+			return errors.New("hostname part must be non-empty and cannot exceed 63 characters")
+		}
+
+		if part[0] == '-' {
+			return errors.New("hostname parts cannot begin with hyphens")
+		}
+
+		if part[len(part)-1] == '-' {
+			return errors.New("hostname parts cannot end with hyphens")
+		}
+
+		for _, r := range part {
+			if (r < 'a' || r > 'z') && (r < '0' || r > '9') && r != '-' {
+				return fmt.Errorf("hostname parts can only contain alphanumeric characters or hyphens, got %q", string(r))
+			}
+		}
+	}
+
+	return nil
+}
+
+func (m *SignupRequest) _validateEmail(addr string) error {
+	a, err := mail.ParseAddress(addr)
+	if err != nil {
+		return err
+	}
+	addr = a.Address
+
+	if len(addr) > 254 {
+		return errors.New("email addresses cannot exceed 254 characters")
+	}
+
+	parts := strings.SplitN(addr, "@", 2)
+
+	if len(parts[0]) > 64 {
+		return errors.New("email address local phrase cannot exceed 64 characters")
+	}
+
+	return m._validateHostname(parts[1])
+}
+
+// SignupRequestMultiError is an error wrapping multiple validation errors
+// returned by SignupRequest.ValidateAll() if the designated constraints
+// aren't met.
+type SignupRequestMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m SignupRequestMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m SignupRequestMultiError) AllErrors() []error { return m }
+
+// SignupRequestValidationError is the validation error returned by
+// SignupRequest.Validate if the designated constraints aren't met.
+type SignupRequestValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SignupRequestValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SignupRequestValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SignupRequestValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SignupRequestValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SignupRequestValidationError) ErrorName() string { return "SignupRequestValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SignupRequestValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSignupRequest.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SignupRequestValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SignupRequestValidationError{}
+
+// Validate checks the field values on SignupResponse with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *SignupResponse) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on SignupResponse with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in SignupResponseMultiError,
+// or nil if none found.
+func (m *SignupResponse) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *SignupResponse) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Id
+
+	if len(errors) > 0 {
+		return SignupResponseMultiError(errors)
+	}
+
+	return nil
+}
+
+// SignupResponseMultiError is an error wrapping multiple validation errors
+// returned by SignupResponse.ValidateAll() if the designated constraints
+// aren't met.
+type SignupResponseMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m SignupResponseMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m SignupResponseMultiError) AllErrors() []error { return m }
+
+// SignupResponseValidationError is the validation error returned by
+// SignupResponse.Validate if the designated constraints aren't met.
+type SignupResponseValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e SignupResponseValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e SignupResponseValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e SignupResponseValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e SignupResponseValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e SignupResponseValidationError) ErrorName() string { return "SignupResponseValidationError" }
+
+// Error satisfies the builtin error interface
+func (e SignupResponseValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sSignupResponse.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = SignupResponseValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = SignupResponseValidationError{}
